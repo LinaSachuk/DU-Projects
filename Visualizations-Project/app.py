8daea9cc78fe2d80dc5b885ba65757c09d641fd5
Lina Sachuk <sachuklina@gmail.com>
2020-04-08 02:53:36 -0600

    added file structure

--- Visualizations-Project/app.py
+++ Visualizations-Project/app.py
@@ -15,10 +15,41 @@ password = urllib.parse.quote_plus('mongo')
 client = MongoClient(
     'mongodb+srv://%s:%s@cluster0-8yire.mongodb.net/test?retryWrites=true&w=majority' % (username, password))
 
+# Connect to the "nobel" database
+db = client.nobel
+# ================================
 
-# db = client.get_database('test_db')
-# mycol = db.test
-# print(mycol.count_documents({}))
+# Create an instance of Flask
+app = Flask(__name__)
+
+
+# Route to render index.html template using data from Mongo
+@app.route("/")
+def home():
+
+    # Find one record of data from the mongo database
+    facts = db.facts.find_one()
+
+    # Return template and data
+    return render_template("index.html", facts=facts)
+
+
+# # Route that will trigger the scrape function
+# @app.route("/scrape")
+# def scrape():
+
+#     # Run the scrape function
+#     mars_data = scrape_mars.scrape_info()
+
+#     # Update the Mongo database using update and upsert=True
+#     mongo.db.collection.update({}, mars_data, upsert=True)
+
+#     # Redirect back to home page
+#     return redirect("/")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
 
 
 # # ==========================================
