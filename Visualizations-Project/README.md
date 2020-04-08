95f5aebd26904dae7b9f5d4d6ec68a5f912086bf
Lina Sachuk <sachuklina@gmail.com>
2020-04-08 01:47:12 -0600

    created mongoDB with 3 collections

---
+++ Visualizations-Project/README.md
@@ -0,0 +1,18 @@
+Project idea - Nobel Prize Winners
+
+Visualization will include a Python Flask–powered RESTful API, HTML/CSS, JavaScript, and MongoDB cloud database.
+
+Will collect the data from the API http://api.nobelprize.org to create a database and https://www.nobelprize.org/ to web-scrape the interesting quick facts.
+Nobel laureates database which we have retrieved as nobel. The database has three collections, prizes, laureates and facts. In the prizes collection, every document correspond to a single Nobel prize, and in the laureates collection - to a single Nobel laureate. The facts collection has a document with a quick facts about history of the Nobel Prize.
+
+Data timeline is between 1901 and 2019, the Nobel Prizes were awarded 597 times to 950 people and organizations.
+
+Data has 6 categories:
+Physics
+Chemistry
+Medicine
+Literature
+Peace
+Economic Sciences
+
+Data has information about winner's name, gender, motivation, country of birth, prizes won ....
--- Visualizations-Project/app.py
+++ Visualizations-Project/app.py
@@ -4,6 +4,8 @@ from flask import Flask, render_template, url_for, template_rendered
 from pymongo import MongoClient
 import urllib.parse
 import json
+import requests
+import csv
 
 
 # ===========================================
@@ -14,9 +16,10 @@ client = MongoClient(
     'mongodb+srv://%s:%s@cluster0-8yire.mongodb.net/test?retryWrites=true&w=majority' % (username, password))
 
 
-db = client.get_database('test_db')
-mycol = db.test
-print(mycol.count_documents({}))
+# db = client.get_database('test_db')
+# mycol = db.test
+# print(mycol.count_documents({}))
+
 
 # # ==========================================
 # # Insert in MongoDB
@@ -74,79 +77,80 @@ print(mycol.count_documents({}))
 # # mycol.delete_many({})
 
 
-# # =================================
-# # Python JSON to dict
+# # # =================================
+# # # Python JSON to dict
 
-person = '{"name": "Bob", "languages": ["English", "Fench"]}'
-person_dict = json.loads(person)
+# person = '{"name": "Bob", "languages": ["English", "Fench"]}'
+# person_dict = json.loads(person)
 
-# Output: {'name': 'Bob', 'languages': ['English', 'Fench']}
-print(person_dict)
+# # Output: {'name': 'Bob', 'languages': ['English', 'Fench']}
+# print(person_dict)
 
-# Output: ['English', 'French']
-print(person_dict['languages'])
+# # Output: ['English', 'French']
+# print(person_dict['languages'])
 
-# ========================================
-# Python read JSON file
-with open('test.json') as f:
-    data = json.load(f)
+# # ========================================
+# # # Python read JSON file
+# # with open('test.json') as f:
+# #     data = json.load(f)
 
-# Output: {'name': 'Bob', 'languages': ['English', 'Fench']}
-print(data)
-
-
-# =========================================
-# Convert dict to JSON
-person_dict = {'name': 'Bob',
-               'age': 12,
-               'children': None
-               }
-person_json = json.dumps(person_dict)
-
-# Output: {"name": "Bob", "age": 12, "children": null}
-print(person_json)
-
-# =========================================
-# Writing JSON to a file
-person_dict = {"name": "Bob",
-               "languages": ["English", "Fench"],
-               "married": True,
-               "age": 32
-               }
-
-with open('person.txt', 'w') as json_file:
-    json.dump(person_dict, json_file)
+# # # Output: {'name': 'Bob', 'languages': ['English', 'Fench']}
+# # print(data)
 
 
-# ==============================
-# Python pretty print JSON
-person_string = '{"name": "Bob", "languages": "English", "numbers": [2, 1.6, null]}'
+# # =========================================
+# # Convert dict to JSON
+# person_dict = {'name': 'Bob',
+#                'age': 12,
+#                'children': None
+#                }
+# person_json = json.dumps(person_dict)
 
-# Getting dictionary
-person_dict = json.loads(person_string)
+# f = open("demofile2.json", "a")
+# f.write(person_json)
+# f.close()
 
-# Pretty Printing JSON string back
-print(json.dumps(person_dict, indent=4, sort_keys=True))
+# # Output: {"name": "Bob", "age": 12, "children": null}
+# print(person_json)
 
-# ===============================================
-# Get all documents from MongoDB and save them to JSON file
+# # =========================================
+# # Writing JSON to a file
+# person_dict = {"name": "Bob",
+#                "languages": ["English", "Fench"],
+#                "married": True,
+#                "age": 32
+#                }
 
-
-host = "Cluster0-shard-0/cluster0-shard-00-00-8yire.mongodb.net:27017,cluster0-shard-00-01-8yire.mongodb.net:27017,cluster0-shard-00-02-8yire.mongodb.net:27017 --ssl"
-username = "mongo"
-password = "mongo"
-directory_to_export = ""
-database = "test_db"
-collection = 'test'
-directory_to_export = '/Users/Lina/DU-Projects/Visualizations-Project'
-file_name = 't'
-
-
-all_collections = []
+# with open('person.txt', 'w') as json_file:
+#     json.dump(person_dict, json_file)
 
 
-command = "mongoexport --host {} --username {} --password {} --db {} --collection {} --pretty --out {}/{}.json"
+# # # ==============================
+# # # Python pretty print JSON
+# # person_string = '{"name": "Bob", "languages": "English", "numbers": [2, 1.6, null]}'
 
-for c in all_collections:
-    os.system(command.format(host, username, password,
-                             database, c, directory_to_export, c))
+# # # Getting dictionary
+# # person_dict = json.loads(person_string)
+
+# # # Pretty Printing JSON string back
+# # print(json.dumps(person_dict, indent=4, sort_keys=True))
+
+# # # ===============================================
+# # # Get all documents from MongoDB and save them to JSON file
+
+
+# # # host = "Cluster0-shard-0/cluster0-shard-00-00-8yire.mongodb.net:27017,cluster0-shard-00-01-8yire.mongodb.net:27017,cluster0-shard-00-02-8yire.mongodb.net:27017"
+# # # username = "mongo"
+# # # password = "mongo"
+# # # directory_to_export = "/Users/Lina/DU-Projects/Visualizations-Project"
+# # # database = "test_db"
+# # # collection = 'test'
+
+
+# # # all_collections = []
+
+
+# # # command = "mongoexport --host {} --username {} --password {} --db {} --collection {} --pretty --out {}/{}.json"
+
+# # # os.system(command.format(host, username, password,
+# # #                          database, collection, directory_to_export, collection))
