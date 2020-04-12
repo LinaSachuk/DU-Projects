c10756543bb124a939d8c8e9dd2535f8e0e1e181
Lina Sachuk <sachuklina@gmail.com>
2020-04-11 23:39:29 -0600

    working html2

--- Visualizations-Project/static/js/logic.js
+++ Visualizations-Project/static/js/logic.js
@@ -9,12 +9,13 @@ d3.json('/prizes').then(function (data) {
     console.log(data);
 })
 
-// mapBounds = L.latLngBounds(southWest, northEast);
-// var map = new L.Map(mapDivName,
-//     {
-//         center: [20, <code>0],
-// 		zoom:</code> 3,
-//             attributionControl: false
-// 	}).addLayer(
-//                 new L.TileLayer("http://{s}.tile2.opencyclemap.org/transport/{z}/{x}/{y}.png")
-//             ).setMaxBounds(mapBounds);
+var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: API_KEY
+}).addTo(mymap);
