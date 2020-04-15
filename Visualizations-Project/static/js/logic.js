a6c14bb628a8f6ed469c58449f139edea6e3206e
Lina Sachuk <sachuklina@gmail.com>
2020-04-15 00:23:28 -0600

    added nobel cards by year

--- Visualizations-Project/static/js/logic.js
+++ Visualizations-Project/static/js/logic.js
@@ -10,6 +10,7 @@ d3.json('../static/data/facts.json', function (response) {
     // console.log(response);
     factst = d3.select('.factst');
     factst.append('p').text(response[0].title);
+    factst.classed('golden', true)
 
     factsp = d3.select('.factsp');
     factsp.append('h5').text(response[0].p);
@@ -21,6 +22,25 @@ d3.json('../static/data/facts.json', function (response) {
 
 })
 
+d3.json('../static/data/countries.json', function (coordinates) {
+    console.log('coordinates:', coordinates)
+    console.log('country name:', coordinates[0].name)
+    console.log('country name:', coordinates[0].latlng)
+
+
+
+})
+
+
+
+
+// reading a laureates json 
+d3.json('../static/data/laureates.json', function (laureates) {
+    console.log('laureates:', laureates)
+    console.log('laureates:', laureates[0].prizes[0].affiliations[0].country)
+
+
+})
 // // Creating map object
 // var map = L.map('map', {
 //     center: [40.7128, -74.0059],
@@ -41,7 +61,7 @@ d3.json('../static/data/facts.json', function (response) {
 
 // creating an anime circle, using JavaScript animation library
 const wrapperEl = document.querySelector('.animeCircle');
-const numberOfEls = 90;
+const numberOfEls = 120;
 const duration = 20000;
 const delay = duration / numberOfEls;
 
@@ -53,16 +73,16 @@ let tl = anime.timeline({
 function createEl(i) {
     let el = document.createElement('div');
     const rotate = (360 / numberOfEls) * i;
-    const translateY = -50;
+    const translateY = -40;
     const hue = Math.round(360 / numberOfEls * i);
     el.classList.add('el');
-    el.style.backgroundColor = 'hsl(' + hue + ', 50%, 60%)';
+    // el.style.backgroundColor = 'hsl(' + hue + ', 50%, 60%)';
     el.style.transform = 'rotate(' + rotate + 'deg) translateY(' + translateY + '%)';
     tl.add({
         begin: function () {
             anime({
                 targets: el,
-                backgroundColor: ['hsl(' + hue + ', 40%, 60%)', 'hsl(' + hue + ', 60%, 80%)'],
+                // backgroundColor: ['hsl(' + hue + ', 40%, 60%)', 'hsl(' + hue + ', 60%, 80%)'],
                 rotate: [rotate + 'deg', rotate + 10 + 'deg'],
                 translateY: [translateY + '%', translateY + 10 + '%'],
                 scale: [1, 1.55],
@@ -108,7 +128,8 @@ anime({
 
 // Use d3.json() to fetch data from JSON file
 // Incoming data is internally referred to as data
-function myPlot(fid = '940') {
+// 1967 1977
+function myPlot(fyear = '1977') {
     d3.json('../static/data/prizes.json', function (data) {
         console.log('data:', data);
 
@@ -122,142 +143,185 @@ function myPlot(fid = '940') {
         categories = categories.filter((v, i, a) => a.indexOf(v) === i);
         console.log('categories:', categories);
 
-        // // Add ids to dropdown menu
-        // for (var i = 0; i < ids.length; i++) {
-        //     selectBox = d3.select('#selDataset');
-        //     selectBox.append('option').text(ids[i]);
-        // }
-
-        // filter sample values by id
-        var sample = data.samples.filter(i => i.id.toString() === fid)[0];
-        console.log(sample);
-
-        var id = sample.id;
-        // console.log('id:', id);
-
-        var washFrequency = data.metadata.filter(i => i.id.toString() === fid)[0]
-            .wfreq;
-        // console.log('washFrequency:', washFrequency);
-
-        // getting sample-metadata
-        var metadata = data.metadata.filter(i => i.id.toString() === fid)[0];
-        // console.log('metadata:', metadata);
-
-        var metadata_card = d3.select('#sample-metadata');
-
-        // refreshing metadata_card
-        metadata_card.html('');
-
-        for (const [key, value] of Object.entries(metadata)) {
-            console.log(key, value);
-            metadata_card.append('p').text(`${key}: ${value}`);
+        // Add ids to dropdown menu
+        for (var i = 0; i < years.length; i++) {
+            selectBox = d3.select('#selDataset');
+            selectBox.append('option').text(years[i]);
         }
 
-        // top 10 OTUs found in that individual
-        // getting sample_values as the values for the bar chart.
-        var sample_values = sample.sample_values.slice(0, 10).reverse();
-        // console.log('sample_values:', sample_values);
+        // filter nobel prizes by year
+        var chosenYear = data.filter(i => i.year.toString() === fyear);
+        console.log('chosenYear:', chosenYear)
 
-        // getting otu_ids as the labels for the bar chart.
-        var otu_ids = sample.otu_ids.slice(0, 10).reverse();
-        console.log('otu_ids:', otu_ids);
 
-        // getting otu_labels as the hovertext for the chart.
-        var otu_labels = otu_ids.map(d => 'OTU ' + d);
-        // console.log('otu_labels:', otu_labels);
+        // Add cards with info for year
+        for (var i = 0; i < chosenYear.length; i++) {
 
-        var trace1 = {
-            x: sample_values,
-            y: otu_labels,
-            text: otu_ids,
-            type: 'bar',
-            orientation: 'h',
-            marker: {
-                color: '#83B588'
-            }
-        };
 
-        // data
-        var chartData = [trace1];
+            d3.select('#prizeByYear')
+                .selectAll("div")
+                .data(chosenYear)
+                .enter()
+                .append("div")
+                .classed("col-lg-3 nobelCards text-center", true)
+                .html(function (d) {
 
-        // Apply the group bar mode to the layout
-        var layout = {
-            title: `Top 10 OTUs found in Subject ${id}`,
-            xaxis: { title: 'Sample Values' },
-            yaxis: { title: '' }
-        };
+                    var firstName = '';
+                    var share = '';
+                    var lastName = '';
+                    var motivation = '';
+                    if (d.laureates) {
+                        share = d.laureates[0].share
 
-        // Render the plot to the div tag with id "plot"
-        Plotly.newPlot('bar', chartData, layout);
+                        if (d.laureates[0].firstname) {
+                            firstName = d.laureates[0].firstname
+                        }
 
-        // Create a bubble chart that displays each sample.
-        // Use otu_ids for the x values.
-        // Use sample_values for the y values.
-        // Use sample_values for the marker size.
-        // Use otu_ids for the marker colors.
-        // Use otu_labels for the text values.
+                        if (d.laureates[0].surname) {
+                            lastName = d.laureates[0].surname
+                        }
 
-        var traceB = {
-            x: otu_ids,
-            y: sample_values,
-            mode: 'markers',
-            marker: {
-                size: sample_values,
-                color: otu_ids
-            },
-            text: otu_labels
-        };
+                        if (d.laureates[0].motivation) {
+                            motivation = d.laureates[0].motivation
+                        }
+                    } else {
+                        motivation = d.overallMotivation
+                    }
 
-        // set the layout for the bubble plot
-        var layoutB = {
-            title: ` Bubble chart for each sample`,
-            xaxis: { title: `OTU ID ${fid}` },
-            tickmode: 'linear',
 
-            yaxis: { title: 'Sample Values' }
-        };
 
-        // creating data variable
-        var dataB = [traceB];
 
-        // create the bubble plot
-        Plotly.newPlot('bubble', dataB, layoutB);
 
-        // the Gauge Chart
-        // part of data to input
-        var data = [
-            {
-                domain: { x: [0, 1], y: [0, 1] },
-                value: washFrequency,
-                type: 'indicator',
-                mode: 'gauge+number',
-                gauge: {
-                    axis: { range: [null, 9] },
-                    bar: { color: '#83A388' },
-                    steps: [
-                        { range: [0, 1], color: '#F8F3EB' },
-                        { range: [1, 2], color: '#F4F1E4' },
-                        { range: [2, 3], color: '#E9E7C8' },
-                        { range: [3, 4], color: '#D5E599' },
-                        { range: [4, 5], color: '#B6CD8F' },
-                        { range: [5, 6], color: '#8AC085' },
-                        { range: [6, 7], color: '#88BB8D' },
-                        { range: [7, 8], color: '#83B588' },
-                        { range: [8, 9], color: '#83A388' }
-                    ]
-                }
-            }
-        ];
+                    return `<h3>${d.year}</h3><hr><h2>${d.category.toUpperCase()}</h2><hr><h4>${firstName} ${lastName}</h4><hr><p>Motivation : ${motivation}</p><p>Share: ${share}</p>`
 
-        var layout = {
-            title: {
-                text: `Belly Button Washing Frequency <br> Scrubs per Week`
-            }
-        };
+                });
 
-        Plotly.newPlot('gauge', data, layout);
+        }
+        //     var year = sample.year;
+        //     // console.log('id:', id);
+
+        //     var washFrequency = data.metadata.filter(i => i.id.toString() === fid)[0]
+        //         .wfreq;
+        //     // console.log('washFrequency:', washFrequency);
+
+        //     // getting sample-metadata
+        //     var metadata = data.metadata.filter(i => i.id.toString() === fid)[0];
+        //     // console.log('metadata:', metadata);
+
+        //     var metadata_card = d3.select('#sample-metadata');
+
+        //     // refreshing metadata_card
+        //     metadata_card.html('');
+
+        //     for (const [key, value] of Object.entries(metadata)) {
+        //         console.log(key, value);
+        //         metadata_card.append('p').text(`${key}: ${value}`);
+        //     }
+
+        //     // top 10 OTUs found in that individual
+        //     // getting sample_values as the values for the bar chart.
+        //     var sample_values = sample.sample_values.slice(0, 10).reverse();
+        //     // console.log('sample_values:', sample_values);
+
+        //     // getting otu_ids as the labels for the bar chart.
+        //     var otu_ids = sample.otu_ids.slice(0, 10).reverse();
+        //     console.log('otu_ids:', otu_ids);
+
+        //     // getting otu_labels as the hovertext for the chart.
+        //     var otu_labels = otu_ids.map(d => 'OTU ' + d);
+        //     // console.log('otu_labels:', otu_labels);
+
+        //     var trace1 = {
+        //         x: sample_values,
+        //         y: otu_labels,
+        //         text: otu_ids,
+        //         type: 'bar',
+        //         orientation: 'h',
+        //         marker: {
+        //             color: '#83B588'
+        //         }
+        //     };
+
+        //     // data
+        //     var chartData = [trace1];
+
+        //     // Apply the group bar mode to the layout
+        //     var layout = {
+        //         title: `Top 10 OTUs found in Subject ${id}`,
+        //         xaxis: { title: 'Sample Values' },
+        //         yaxis: { title: '' }
+        //     };
+
+        //     // Render the plot to the div tag with id "plot"
+        //     Plotly.newPlot('bar', chartData, layout);
+
+        //     // Create a bubble chart that displays each sample.
+        //     // Use otu_ids for the x values.
+        //     // Use sample_values for the y values.
+        //     // Use sample_values for the marker size.
+        //     // Use otu_ids for the marker colors.
+        //     // Use otu_labels for the text values.
+
+        //     var traceB = {
+        //         x: otu_ids,
+        //         y: sample_values,
+        //         mode: 'markers',
+        //         marker: {
+        //             size: sample_values,
+        //             color: otu_ids
+        //         },
+        //         text: otu_labels
+        //     };
+
+        //     // set the layout for the bubble plot
+        //     var layoutB = {
+        //         title: ` Bubble chart for each sample`,
+        //         xaxis: { title: `OTU ID ${fid}` },
+        //         tickmode: 'linear',
+
+        //         yaxis: { title: 'Sample Values' }
+        //     };
+
+        //     // creating data variable
+        //     var dataB = [traceB];
+
+        //     // create the bubble plot
+        //     Plotly.newPlot('bubble', dataB, layoutB);
+
+        //     // the Gauge Chart
+        //     // part of data to input
+        //     var data = [
+        //         {
+        //             domain: { x: [0, 1], y: [0, 1] },
+        //             value: washFrequency,
+        //             type: 'indicator',
+        //             mode: 'gauge+number',
+        //             gauge: {
+        //                 axis: { range: [null, 9] },
+        //                 bar: { color: '#83A388' },
+        //                 steps: [
+        //                     { range: [0, 1], color: '#F8F3EB' },
+        //                     { range: [1, 2], color: '#F4F1E4' },
+        //                     { range: [2, 3], color: '#E9E7C8' },
+        //                     { range: [3, 4], color: '#D5E599' },
+        //                     { range: [4, 5], color: '#B6CD8F' },
+        //                     { range: [5, 6], color: '#8AC085' },
+        //                     { range: [6, 7], color: '#88BB8D' },
+        //                     { range: [7, 8], color: '#83B588' },
+        //                     { range: [8, 9], color: '#83A388' }
+        //                 ]
+        //             }
+        //         }
+        //     ];
+
+        //     var layout = {
+        //         title: {
+        //             text: `Belly Button Washing Frequency <br> Scrubs per Week`
+        //         }
+        //     };
+
+        //     Plotly.newPlot('gauge', data, layout);
     });
 }
 
-myPlot();
-
+myPlot()
